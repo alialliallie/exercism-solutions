d8d386e8bff66f42065c66dc9df1fa42c5f9ecb9
Allison Keene <allison@alliesaur.us>
2015-05-10 11:52:10 -0700

    Clean up nth(count)
    http://exercism.io/submissions/45e81104408f4302ad023c26204c9b12

--- ruby/nth-prime/nth_prime.rb
+++ ruby/nth-prime/nth_prime.rb
@@ -3,8 +3,16 @@ class Prime
     raise ArgumentError if prime_count == 0
     primes = [2]
 
-    # start with 3, already covered primes less than this w/ priming
-    odds = Enumerator.new do |yielder|
+    odds.lazy.take_while { primes.length < prime_count }.each do |i|
+      primes << i unless primes.any? { |p| (i % p) == 0 }
+    end
+    primes.last
+  end
+
+  private
+  # start with 3, already covered primes less than this w/ priming
+  def odds
+    Enumerator.new do |yielder|
       n = 3 
       loop do
         yielder.yield n
@@ -12,12 +20,5 @@ class Prime
       end
     end
 
-    odds.lazy.take_while { primes.length < prime_count }.each do |i|
-      has_factor = primes.any? do |p|
-        (i % p) == 0
-      end
-      primes << i unless has_factor
-    end
-    primes[prime_count - 1]
   end
 end
