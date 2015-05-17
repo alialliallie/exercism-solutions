4c0221705466e45133d2a818161d3b02571efabf
Allison Keene <allison@alliesaur.us>
2015-05-17 09:49:14 -0700

    Clean up food chain.
    Remove a bunch of redundancy.
    
     http://exercism.io/submissions/9026a4235c7d42a6bf9702f5af6b690a

--- ruby/food-chain/food_chain.rb
+++ ruby/food-chain/food_chain.rb
@@ -1,6 +1,4 @@
 class FoodChainSong
-  attr_accessor :verses
-
   ORDER = [nil] + %w(fly spider bird cat dog goat cow horse)
 
   VERSE = {
@@ -14,47 +12,44 @@ class FoodChainSong
     "fly" => "I don't know why she swallowed the fly. Perhaps she'll die."
   }
 
-  WHATEVER = {
-    "bird" => "How absurd to swallow a bird!",
-    "spider" => "that wriggled and jiggled and tickled inside her",
-    "fly" => "I don't know why she swallowed the fly. Perhaps she'll die."
-  }
-
-  def initialize
-  end
-
   def sing
     verses(1,8)
   end
 
   def verses(first, last)
     verses = first.upto(last).map {|v| verse(v) }
-    verses.join("\n") + "\n"
+    trailing_join(verses)
   end
 
   def verse(n)
-    thing = ORDER[n]
-    lines = stanza(n)
-    unless thing == "horse" 
+    food = ORDER[n]
+    lines = stanza(food)
+    unless food == "horse" 
       n.downto(2).each do |i|
-        lines << swallowed_why(i, i-1, n)
+        lines << swallowed_why(ORDER[i], ORDER[i-1])
       end
       lines << VERSE["fly"] unless n == 1
     end
-    lines.join("\n") + "\n"
+    trailing_join(lines)
   end
 
   private
-  def stanza(n)
-    what = ORDER[n]
-    [ "I know an old lady who swallowed a #{what}.",
-      VERSE[what] ]
+  def stanza(food)
+    [ "I know an old lady who swallowed a #{food}.",
+      VERSE[food] ]
   end
 
-  def swallowed_why(swallowed, target, n)
-    x = ORDER[swallowed]
-    y = ORDER[target]
-    appendix = " " + WHATEVER[y] if target == 2
-    "She swallowed the #{x} to catch the #{y}#{appendix||""}."
+  def swallowed_why(food, target)
+    if target == "spider"
+      extra = VERSE[target].gsub(/^It/,"that")
+      "She swallowed the #{food} to catch the #{target} #{extra}"
+    else
+      "She swallowed the #{food} to catch the #{target}."
+    end
+  end
+
+  # Join lines with trailing \n per spec
+  def trailing_join(lines)
+    lines.join("\n") + "\n"
   end
 end
