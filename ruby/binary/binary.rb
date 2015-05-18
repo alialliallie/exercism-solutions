660f70bc19d9f99820e56b4d7172379304798339
Allison Keene <allison@alliesaur.us>
2015-05-17 21:17:20 -0700

    Binary conversion from string
    http://exercism.io/submissions/4f924eb0fff14aff9302032c6119c782

---
+++ ruby/binary/binary.rb
@@ -0,0 +1,12 @@
+class Binary
+  def initialize(binary_string)
+    @binary = binary_string
+  end
+
+  def to_decimal
+    return 0 if @binary.match /[^01]/
+    @binary.chars.reverse.each_with_index.reduce(0) do |sum, (bit, place)|
+      sum + 2**place.to_i * bit.to_i
+    end
+  end
+end
