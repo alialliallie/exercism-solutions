e71a3034ccbe9cdadd0981cce91f1c5c0e95f2bd
Allison Keene <allison@alliesaur.us>
2015-05-17 21:31:01 -0700

    Rework robot name
     http://exercism.io/submissions/67c4c0cffb654b2e809710a99e54612c

--- ruby/robot-name/robot_name.rb
+++ ruby/robot-name/robot_name.rb
@@ -1,18 +1,18 @@
 class Robot
-  def name
-    @name ||= "#{prefix}#{designation}"
+  attr_reader :name
+
+  def initialize
+    @name = rename
   end
 
   def reset
-    @name = nil
+    @name = rename
   end
 
   private
-  def prefix
-    ("A".."Z").to_a.shuffle[0,2].join
-  end
-
-  def designation
-    "%03d" % rand(999) 
+  def rename
+    prefix = ("A".."Z").to_a.shuffle[0,2].join
+    designation = "%03d" % rand(999) 
+    @name = "#{prefix}#{designation}"
   end
 end
