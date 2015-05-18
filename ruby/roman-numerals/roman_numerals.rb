a1796eeb019890aade02627514c9309bc6c4e130
Allison Keene <allison@alliesaur.us>
2015-05-17 20:30:02 -0700

    Iterate numerals not magnitudes.
     http://exercism.io/submissions/11e1d4a0e633487e840aad07d03a30d0

--- ruby/roman-numerals/roman_numerals.rb
+++ ruby/roman-numerals/roman_numerals.rb
@@ -1,44 +1,17 @@
 class Fixnum
-  ROMAN_NUMERALS = {
-    1 => 'I',
-    4 => 'IV',
-    5 => 'V',
-    9 => 'IX',
-    10 => 'X',
-    40 => 'XL',
-    50 => 'L',
-    90 => 'XC',
-    100 => 'C',
-    400 => 'CD',
-    500 => 'D',
-    900 => 'CM',
-    1000 => 'M'
-  }
+  ROMAN_NUMERALS = [
+    [1, 'I'], [4, 'IV'], [5, 'V'], [9, 'IX'],
+    [10, 'X'], [40, 'XL'], [50, 'L'], [90, 'XC'],
+    [100, 'C'], [400, 'CD'], [500, 'D'], [900, 'CM'],
+    [1000, 'M']
+  ].reverse
 
   def to_roman
-    out = ''
-    remainder = [1000, 100, 10].reduce(self) do |memo, mag|
-      digit = memo / mag
-      unless digit == 0
-        out << roman_numeral_from_digit(digit, mag)
-      end
-      memo - (memo / mag * mag)
-    end
-    out << roman_numeral_from_digit(remainder, 1) if remainder > 0
-    out
-  end
-
-  private
-
-  def roman_numeral_from_digit(d, m)
-    return ROMAN_NUMERALS[d * m] if ROMAN_NUMERALS[d * m]
-    numeral = ROMAN_NUMERALS[m]
-    next_numeral = ROMAN_NUMERALS[m * 5]
-
-    if d < 5
-      numeral * d
-    else
-      next_numeral + (numeral * (d - 5))
+    n = self
+    ROMAN_NUMERALS.reduce('') do |o, pair|
+      div, mod = n.divmod(pair[0])
+      n = mod
+      o << pair[1] * div
     end
   end
 end
