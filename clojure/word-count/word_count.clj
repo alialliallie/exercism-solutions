bb312439c2fefedfa70b913e61542cf721db0ecb
Allison Keene <allison@alliesaur.us>
2015-05-23 16:34:56 -0700

    Count the words
    http://exercism.io/submissions/b07ce2454775490491b5f040bd8f4047

---
+++ clojure/word-count/word_count.clj
@@ -0,0 +1,20 @@
+(ns word-count
+  (:refer-clojure :exclude [replace])
+  (:require [clojure.string :refer [split lower-case replace blank?]]))
+
+(defn- not-blank?
+  [s]
+  (not (blank? s)))
+
+(defn- prepare
+  [s]
+  (filter not-blank?
+          (map #(replace % #"\W+$" "")
+               (split (lower-case s) #" "))))
+
+(defn word-count
+  [phrase]
+  (let [words (prepare phrase)]
+    (into {}
+          (map (fn [[word list]] [word (count list)])
+               (group-by identity words)))))
