94c8d13209d1b00f05254ab2c2491906a9622b5f
Allison Keene <allison@alliesaur.us>
2015-06-01 21:16:04 -0700

    Ruby: binary readability

--- ruby/binary/binary.rb
+++ ruby/binary/binary.rb
@@ -5,8 +5,9 @@ class Binary
 
   def to_decimal
     return 0 if @binary.match(/[^01]/)
-    @binary.chars.reverse.each_with_index.reduce(0) do |sum, (bit, place)|
-      sum + 2**place.to_i * bit.to_i
-    end
+    @binary.chars.reverse.each_with_index
+           .reduce(0) do |sum, (bit, place)|
+             sum + 2**place.to_i * bit.to_i
+           end
   end
 end
