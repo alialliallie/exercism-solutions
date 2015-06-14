1e2e40c2ed7691707554c46bccc65d1319082910
Allison Keene <allison@alliesaur.us>
2015-06-13 20:22:54 -0700

    Ruby: space-age
    metaprogrammed

--- ruby/space-age/space_age.rb
+++ ruby/space-age/space_age.rb
@@ -1,44 +1,4 @@
 class SpaceAge
-  attr_accessor :seconds
-
-  def initialize(seconds)
-    @seconds = seconds
-  end
-
-  def on_earth
-    age_on(:earth)
-  end
-
-  def on_mercury
-    age_on(:mercury)
-  end
-
-  def on_venus
-    age_on(:venus)
-  end
-
-  def on_mars
-    age_on(:mars)
-  end
-
-  def on_jupiter
-    age_on(:jupiter)
-  end
-
-  def on_saturn
-    age_on(:saturn)
-  end
-
-  def on_uranus
-    age_on(:uranus)
-  end
-
-  def on_neptune
-    age_on(:neptune)
-  end
-
-  private
-
   EARTH_PERIOD = 31_557_600
   ORBITAL = {
     mercury: EARTH_PERIOD * 0.2408467,
@@ -50,6 +10,20 @@ class SpaceAge
     uranus: EARTH_PERIOD * 84.016846,
     neptune: EARTH_PERIOD * 164.79132
   }
+
+  attr_accessor :seconds
+
+  def initialize(seconds)
+    @seconds = seconds
+  end
+
+  ORBITAL.keys.each do |orbit|
+    define_method("on_#{orbit}") do
+      age_on(orbit)
+    end
+  end
+
+  private
 
   def age_on(planet)
     seconds.to_f / ORBITAL[planet]
