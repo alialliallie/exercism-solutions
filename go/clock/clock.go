e55218ad36c5ba2f5fad9ee69161aed7744c2497
Allison Keene <allison@alliesaur.us>
2015-06-17 08:25:31 -0700

    Go: clock
    ugh I hate time

---
+++ go/clock/clock.go
@@ -0,0 +1,42 @@
+package clock
+
+import "fmt"
+
+const TestVersion = 2
+const wholeDay = 24 * 60
+
+func divmod(x, y int) (int, int) {
+    return x / y, x % y
+}
+type Clock struct {
+    hour int
+    minute int
+}
+
+func Time(hour, minute int) Clock {
+    var midnight = Clock{0, 0}
+    var allMinutes = (hour * 60) + minute
+    return midnight.Add(allMinutes)
+}
+
+func (c Clock) minutes() int {
+    return c.hour * 60 + c.minute
+}
+
+func (c Clock) String() string {
+    if (c.hour == 24) {
+        return fmt.Sprintf("00:%02d", c.minute)
+    } else {
+        return fmt.Sprintf("%02d:%02d", c.hour, c.minute)
+    }
+}
+
+func (c Clock) Add(minute int) Clock {
+    var minutes = (c.minutes() + minute) % wholeDay
+    if (minutes < 0) {
+        minutes = wholeDay + minutes
+    }
+    var h, m = divmod(minutes, 60)
+    return Clock{h % 24, m}
+}
+
