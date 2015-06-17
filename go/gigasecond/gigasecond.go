bd197ce3e20b0674042bedf70488e490e49db6a2
Allison Keene <allison@alliesaur.us>
2015-06-17 09:29:38 -0700

    Go: gigasecond
    I kind of forgot how annoying types can be

---
+++ go/gigasecond/gigasecond.go
@@ -0,0 +1,16 @@
+package gigasecond
+
+import "time"
+import "math"
+
+const TestVersion = 2
+
+var Birthday = time.Date(1980, 4, 3, 21, 30, 0, 0, time.UTC)
+
+func gigasecond() time.Duration {
+	return time.Duration(math.Pow(10, 9)) * time.Second
+}
+
+func AddGigasecond(time time.Time) time.Time {
+	return time.Add(gigasecond())
+}
