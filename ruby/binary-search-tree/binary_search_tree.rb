632b6386832be4b6a5a184da0317391018d367fa
Allison Keene <allison@alliesaur.us>
2015-06-19 19:35:16 -0700

    Clearer ruby BST code

--- ruby/binary-search-tree/binary_search_tree.rb
+++ ruby/binary-search-tree/binary_search_tree.rb
@@ -5,23 +5,32 @@ class Bst
   end
 
   def insert(n)
-    @data = n and return if @data.nil?
+    # Allow leaf nodes to be assigned a value later
+    # so that inserts can chain through each parent
+    if @data.nil?
+      @data = n
+    else
+      insert_child(n)
+    end
+  end
+
+  def each
+    return enum_for(:each) unless block_given?
+
+    left.each { |n| yield n } if left
+    yield data
+    right.each { |n| yield n } if right
+  end
+
+  private
+
+  def insert_child(n)
     if n <= data
       @left ||= Bst.new
       @left.insert(n)
     else
       @right ||= Bst.new
       @right.insert(n)
-    end
-  end
-
-  def each
-    if left
-      left.each { |n| yield n }
-    end
-    yield data
-    if right
-      right.each { |n| yield n }
     end
   end
 end
