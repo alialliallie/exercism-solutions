ce7da3afdb27c14f2a4cf8a10195c1a1596dcdab
Allison Lara <allison@alliesaur.us>
2015-08-08 10:47:00 -0700

    Speed up ruby nth-prime

--- ruby/nth-prime/nth_prime.rb
+++ ruby/nth-prime/nth_prime.rb
@@ -1,24 +1,30 @@
 class Prime
   def nth(prime_count)
-    raise ArgumentError if prime_count == 0
+    fail ArgumentError if prime_count == 0
     primes = [2]
 
     odds.lazy.take_while { primes.length < prime_count }.each do |i|
-      primes << i unless primes.any? { |p| (i % p) == 0 }
+      primes << i unless existing(primes, i)
     end
     primes.last
   end
 
   private
+
   # start with 3, already covered primes less than this w/ priming
   def odds
     Enumerator.new do |yielder|
-      n = 3 
+      n = 3
       loop do
         yielder.yield n
-        n = n + 2
+        n += 2
       end
     end
+  end
 
+  def existing(primes, i)
+    root_i = Math.sqrt(i)
+    primes.take_while { |p| p <= root_i }
+      .any? { |p| (i % p) == 0 }
   end
 end
