362f19a6aaed87c9548a204577912758a552ec12
Allison Lara <allison@alliesaur.us>
2015-08-08 15:34:43 -0700

    clojure anagram sort not freq

--- clojure/anagram/anagram.clj
+++ clojure/anagram/anagram.clj
@@ -1,18 +1,13 @@
 (ns anagram
   (:require [clojure.string :as s]))
 
-(defn- letter-count
-  [word]
-  (frequencies (seq (s/lower-case word))))
-
 (defn- anagram?
   [word candidate]
   (let [w (s/lower-case word)
         c (s/lower-case candidate)]
     (and (not= w c)
-         (= (letter-count w) (letter-count c)))))
+         (= (sort w) (sort c)))))
 
 (defn anagrams-for
   [word candidates]
-  (let [word-count (letter-count word)]
-    (filter (partial anagram? word) candidates)))
+    (filter (partial anagram? word) candidates))
