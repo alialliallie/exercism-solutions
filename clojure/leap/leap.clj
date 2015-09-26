a11e5c0e36f979dbd736c53224f2415c558171e6
Allison Keene <allison@alliesaur.us>
2015-09-26 09:25:28 -0700

    condp in leap

--- clojure/leap/leap.clj
+++ clojure/leap/leap.clj
@@ -6,8 +6,8 @@
 
 (defn leap-year?
   [year]
-  (cond
-    (divides-by? year 400) true
-    (divides-by? year 100) false
-    (divides-by? year 4) true
-    :else false))
+  (condp #(divides-by? %2 %1) year
+    400 true
+    100 false
+    4   true
+    false))
