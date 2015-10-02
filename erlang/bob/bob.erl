09c93eac634551d61d25700c53d05e9c32e209b0
Allison Lara <allison@alliesaur.us>
2015-10-02 14:19:14 -0700

    Erlang: Bob

---
+++ erlang/bob/bob.erl
@@ -0,0 +1,35 @@
+-module(bob).
+%% -export([response_for/1]).
+-compile([export_all]).
+
+response_for(Remark) ->
+    response_for_shout(Remark, is_shout(Remark)).
+
+response_for_question(_, true) ->
+    "Sure.";
+response_for_question(Remark, false) ->
+    response_for_silence(Remark, is_silence(Remark)).
+
+response_for_shout(_, true) ->
+    "Whoa, chill out!";
+response_for_shout(Remark, false) ->
+    response_for_question(Remark, is_question(Remark)).
+
+response_for_silence(_, true) ->
+    "Fine. Be that way!";
+response_for_silence(_, false) ->
+    "Whatever.".
+
+is_question([]) -> false;
+is_question(Remark) -> 
+    hd("?") == hd(lists:reverse(Remark)).
+
+is_shout(Remark) ->
+    Remark =:= string:to_upper(Remark) andalso
+    Remark =/= string:to_lower(Remark).
+
+is_silence(Remark) -> 
+    silence_tokens(string:tokens(Remark, " ")).
+
+silence_tokens([]) -> true;
+silence_tokens(_) -> false.
