4e02fedc49873589be8ffd95a35bc5a60b57af32
Allison Lara <allison@alliesaur.us>
2015-10-07 13:05:46 -0700

    Erlang point mutations quick lint update

--- erlang/point-mutations/dna.erl
+++ erlang/point-mutations/dna.erl
@@ -9,5 +9,5 @@ hamming_distance(_,  [], Acc) -> Acc;
 hamming_distance([],  _, Acc) -> Acc;
 hamming_distance([LH|LT], [RH|RT], Acc) when LH =/= RH ->
     hamming_distance(LT, RT, Acc + 1);
-hamming_distance([LH|LT], [RH|RT], Acc) ->
+hamming_distance([_|LT], [_|RT], Acc) ->
     hamming_distance(LT, RT, Acc).
