5dab148bd26dc872a9421d0d56f302a675508a1d
Allison Lara <allison@alliesaur.us>
2015-10-20 11:28:52 -0700

    Remove use of case in erlang exercises.

--- erlang/strain/strain.erl
+++ erlang/strain/strain.erl
@@ -5,11 +5,9 @@ keep(F, List) ->
     keep(F, List, []).
 
 keep(_, [], Acc) -> lists:reverse(Acc);
-keep(F, [H|T], Acc) ->
-    Keep = F(H),
-    case Keep of
-        true -> keep(F, T, [H|Acc]);
-        false -> keep(F, T, Acc)
-    end.
+keep(F, [H|T], Acc) -> keep_if(F(H), F, [H|T], Acc).
+
+keep_if(true, F, [H|T], Acc) -> keep(F, T, [H|Acc]);
+keep_if(false, F, [_|T], Acc) -> keep(F, T, Acc).
 
 discard(F, List) -> keep(fun(X) -> not F(X) end, List).
--- erlang/sum-of-multiples/sum_of_multiples.erl
+++ erlang/sum-of-multiples/sum_of_multiples.erl
@@ -3,13 +3,15 @@
 
 sumOfMultiplesDefault(N) -> sumOfMultiples([3,5], N).
 
-sumOfMultiples(Multiples, N) -> 
+sumOfMultiples(Multiples, N) ->
     sumOfMultiples(Multiples, N - 1, 0).
 
 sumOfMultiples(_, 0, Acc) -> Acc;
 sumOfMultiples(Multiples, N, Acc) ->
     Factor = lists:any(fun (M) -> N rem M =:= 0 end, Multiples),
-    case Factor of
-        true -> sumOfMultiples(Multiples, N - 1, Acc + N);
-        false -> sumOfMultiples(Multiples, N - 1, Acc)
-    end.
+    sum_if(Factor, Multiples, N, Acc).
+
+sum_if(true, Multiples, N, Acc) ->
+    sumOfMultiples(Multiples, N - 1, Acc + N);
+sum_if(false, Multiples, N, Acc) ->
+    sumOfMultiples(Multiples, N - 1, Acc).
