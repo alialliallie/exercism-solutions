47049d7dc81af1220edaabb32186b2269c836efc
Allison Lara <allison@alliesaur.us>
2015-11-18 15:37:11 -0800

    Erlang: point mutations improvement
    Use lists functions for zip/count

--- erlang/point-mutations/dna.erl
+++ erlang/point-mutations/dna.erl
@@ -2,12 +2,9 @@
 -export([hamming_distance/2]).
 
 hamming_distance(L, R) -> 
-    hamming_distance(L, R, 0).
+    hamming_distance(lists:zip(L, R)).
 
-hamming_distance([], [], Acc) -> Acc;
-hamming_distance(_,  [], Acc) -> Acc;
-hamming_distance([],  _, Acc) -> Acc;
-hamming_distance([LH|LT], [RH|RT], Acc) when LH =/= RH ->
-    hamming_distance(LT, RT, Acc + 1);
-hamming_distance([_|LT], [_|RT], Acc) ->
-    hamming_distance(LT, RT, Acc).
+hamming_distance(Zip) ->
+    length(lists:filter(fun({L, R}) ->
+                               L =/= R
+                        end, Zip)).
