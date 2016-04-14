d4bf2e47c1ec3f4859f3b6418c38ee5cf4ca0e5e
Allison Lara <allison@alliesaur.us>
2016-04-14 08:51:45 -0700

    Extra join is silly

--- ruby/pangram/pangram.rb
+++ ruby/pangram/pangram.rb
@@ -1,7 +1,7 @@
 module Pangram
   VERSION = 1
-  ALPHA = ('a'..'z').to_a.join
+  ALPHA = ('a'..'z').to_a
   def self.is_pangram?(sentence)
-    sentence.downcase.gsub(/[^a-z]/, '').chars.uniq.sort.join == ALPHA
+    sentence.downcase.gsub(/[^a-z]/, '').chars.uniq.sort == ALPHA
   end
 end
