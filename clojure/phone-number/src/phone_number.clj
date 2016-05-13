47bfed74293aedbe8415cdfc278713fa307db240
Allison Lara <allison@alliesaur.us>
2016-05-13 09:56:15 -0700

    clojure: refactor phone-number

--- clojure/phone-number/src/phone_number.clj
+++ clojure/phone-number/src/phone_number.clj
@@ -3,29 +3,46 @@
 
 (def invalid "0000000000")
 
+(defn- valid?
+  [phone]
+  (if (= 11 (.length phone))
+    (.startsWith phone "1")
+    (= 10 (.length phone))))
+
+(def not-valid? (complement valid?))
+
+(defn- str-tail
+  "Get the last n characters of a string."
+  [string n]
+  (let [len (.length string)]
+    (.substring string (- len n))))
+
 (defn- with-area-code?
-  [num]
-  (and (= (count num) 11) (= \1 (first num))))
+  [number]
+  (and (= (count number) 11) (.startsWith number "1")))
 
-(defn number
+(defn- clean
   [phone]
-  (let [num (s/replace phone #"\D" "")]
-    (cond
-      (with-area-code? num) (apply str (rest num))
-      (not= (count num) 10) invalid
-      :else num)))
-
-(defn area-code
-  [phone]
-  (apply str (take 3 (number phone))))
+  (let [digits (s/replace phone #"\D" "")]
+    (if (not-valid? digits) invalid digits)))
 
 (defn- format-number
   [num]
-  (let [numbers (take-last 7 num)]
-    (str (apply str (take 3 numbers))
-         "-" (apply str (take-last 4 numbers)))))
+  (let [numbers (str-tail num 7)]
+    (str (.substring numbers 0 3) "-" (str-tail numbers 4))))
+
+(defn number
+  [phone]
+  (let [clean-phone (clean phone)]
+    (if (with-area-code? clean-phone)
+      (.substring clean-phone 1)
+      clean-phone)))
+
+(defn area-code
+  [phone]
+  (.substring (number phone) 0 3))
 
 (defn pretty-print
   [phone]
-  (str "(" (area-code phone)") "
-       (format-number (number phone))))
+  (let [n (number phone)]
+    (str "(" (area-code n) ") " (format-number n))))
