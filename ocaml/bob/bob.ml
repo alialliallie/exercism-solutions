338f48afa0d5a54da17655981596ab2d1529cd5e
Allison Lara <allison@alliesaur.us>
2016-06-28 13:53:43 -0700

    OCaml: bob curry it all

--- ocaml/bob/bob.ml
+++ ocaml/bob/bob.ml
@@ -1,18 +1,16 @@
 open Core.Std
 
-let is_question remark =
-    String.is_suffix remark "?"
+let is_question = String.is_suffix ~suffix:"?"
 
 let is_yelling remark =
-    let has_alpha = String.exists ~f:Char.is_alpha remark in
-        has_alpha && String.uppercase remark = remark
+  let has_alpha = String.exists ~f:Char.is_alpha remark in
+  has_alpha && String.uppercase remark = remark
 
-let is_silence remark =
-    String.is_empty(String.strip remark)
+let is_silence = String.for_all ~f:Char.is_whitespace
 
 let response_for remark =
-    match remark with
-    | _ when is_silence remark -> "Fine. Be that way!"
-    | _ when is_yelling remark -> "Whoa, chill out!"
-    | _ when is_question remark  -> "Sure."
-    | _ -> "Whatever."
+  match remark with
+  | _ when is_silence remark -> "Fine. Be that way!"
+  | _ when is_yelling remark -> "Whoa, chill out!"
+  | _ when is_question remark  -> "Sure."
+  | _ -> "Whatever."
