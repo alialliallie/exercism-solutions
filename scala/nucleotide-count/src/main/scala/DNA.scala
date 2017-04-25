756b28afec39eb9f9dc0c084431991e3fdfb7307
Allison Lara <allison@alliesaur.us>
2017-04-25 10:19:33 -0700

    Scala: nucleotide count monads

--- scala/nucleotide-count/src/main/scala/DNA.scala
+++ scala/nucleotide-count/src/main/scala/DNA.scala
@@ -1,15 +1,12 @@
-import java.util.NoSuchElementException
-
 class DNA(dna: String) {
-  val valid = Set('A', 'C', 'G', 'T')
+  val valid = Set('A', 'T', 'C', 'G')
   val baseCounts = Map('A' -> 0, 'T' -> 0, 'C' -> 0, 'G' -> 0)
 
   def count(nucleotide: Char): Either[String, Int] =
-    try {
-      nucleotideCounts.right.map(counts => counts(nucleotide))
-    } catch {
-      case _: NoSuchElementException => Left(invalidMsg(nucleotide))
-    }
+    for {
+      n <- validNucleotide(nucleotide)
+      counts <- nucleotideCounts
+    } yield counts(n)
 
   def nucleotideCounts: Either[String, Map[Char, Int]] =
     invalid match {
@@ -17,7 +14,12 @@ class DNA(dna: String) {
       case Some(nucleotide) => Left(invalidMsg(nucleotide))
     }
 
-  private def invalid = dna.find(!valid.contains(_))
+  private def validNucleotide(nucleotide: Char): Either[String, Char] =
+    Either.cond(valid.contains(nucleotide),
+      nucleotide,
+      invalidMsg(nucleotide))
+
+  private def invalid: Option[Char] = dna.find(!valid.contains(_))
   private def invalidMsg(nucleotide: Char): String =
    s"invalid nucleotide '$nucleotide'"
 }
