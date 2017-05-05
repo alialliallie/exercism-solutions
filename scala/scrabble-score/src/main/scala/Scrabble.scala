b5740ef5c9e99e55019b8a5f85c4f4ced7a94b84
Allison Lara <allison@alliesaur.us>
2017-05-05 10:32:35 -0700

    Scala: Scrabble score

---
+++ scala/scrabble-score/src/main/scala/Scrabble.scala
@@ -0,0 +1,15 @@
+object Scrabble {
+  val scores = Map(
+    1 -> Seq('A', 'E', 'I', 'O', 'U', 'L', 'N', 'R', 'S', 'T'),
+    2 -> Seq('D', 'G'),
+    3 -> Seq('B', 'C', 'M', 'P'),
+    4 -> Seq('F', 'H', 'V', 'W', 'Y'),
+    5 -> Seq('K'),
+    8 -> Seq('J', 'X'),
+    10 -> Seq('Q', 'Z')
+  ).flatMap { case (s, ls) => ls.map { _ -> s} }
+
+  def scoreLetter(letter: Char): Int = scores(letter.toUpper)
+
+  def scoreWord(word: String): Int = word.toUpperCase.map(scores(_)).reduce(_ + _)
+}
