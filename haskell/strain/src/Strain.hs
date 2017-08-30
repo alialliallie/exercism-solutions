f4189fa22153a450ef53e0e43d5222538959352e
Allison Lara <allison@alliesaur.us>
2017-08-30 12:46:49 -0700

    fix haskell strain

--- haskell/strain/src/Strain.hs
+++ haskell/strain/src/Strain.hs
@@ -4,8 +4,5 @@ discard :: (a -> Bool) -> [a] -> [a]
 discard p = keep (not . p)
 
 keep :: (a -> Bool) -> [a] -> [a]
-keep = doKeep []
-
-doKeep :: [a] -> (a -> Bool) -> [a] -> [a]
-doKeep acc p = foldr (\a b -> if p a then a:b else b) []
+keep p = foldr (\a b -> if p a then a:b else b) []
 
